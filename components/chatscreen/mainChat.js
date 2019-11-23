0582c728315921f33e03e3d82344dd584fbd4e9a
vishnu <vishnuedavarath@gmail.com>
2019-11-23 11:46:52 +0530

    Change in Indentation

--- components/chatscreen/mainChat.js
+++ components/chatscreen/mainChat.js
@@ -1,50 +1,52 @@
 import React from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import { View, KeyboardAvoidingView } from "react-native";
-import { Header } from 'react-native-elements';
+import { Header } from "react-native-elements";
 
 export default class ChatView extends React.Component {
-  state = {
-    messages: [],
-    user: "John Doe"
+    state = {
+        messages: [],
+        user: "John Doe"
+    };
 
-  };
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: "Hi",
+                    createdAt: new Date(),
+                    user: {
+                        _id: 1,
+                        name: "React Native",
+                        avatar: "https://placeimg.com/140/140/any"
+                    }
+                }
+            ]
+        });
+    }
 
-  componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: "Hi",
-          createdAt: new Date(),
-          user: {
-            _id: 1,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any"
-          }
-        }
-      ]
-    });
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <Header
-          //   leftComponent={{ icon: "menu", color: "#fff" }}
-          leftComponent={{ text: this.state.user.name, style: { color: "#fff" } }}
-          rightComponent={{ icon: "more-vert", color: "#fff" }}
-        />
-        <GiftedChat
-          messages={this.state.messages}
-          alwaysShowSend={true}
-          isAnimated={true}
-          user={{
-            _id: this.user_id
-          }}
-        />
-        <KeyboardAvoidingView behavior="padding" />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <Header
+                    //   leftComponent={{ icon: "menu", color: "#fff" }}
+                    leftComponent={{
+                        text: this.state.user.name,
+                        style: { color: "#fff" }
+                    }}
+                    rightComponent={{ icon: "more-vert", color: "#fff" }}
+                />
+                <GiftedChat
+                    messages={this.state.messages}
+                    alwaysShowSend={true}
+                    isAnimated={true}
+                    user={{
+                        _id: this.user_id
+                    }}
+                />
+                <KeyboardAvoidingView behavior="padding" />
+            </View>
+        );
+    }
 }
--- components/msgscreen/mainMsgScreen.js
+++ components/msgscreen/mainMsgScreen.js
@@ -1,46 +1,44 @@
 import React, { Component } from "react";
 import { View } from "react-native";
-import { Header } from "react-native-elements";
+// import { Header } from "react-native-elements";
 import {
-  Container,
-  Content,
-  List,
-  ListItem,
-  Left,
-  Body,
-  Right,
-  Thumbnail,
-  Text
+    Container,
+    Header,
+    Content,
+    List,
+    ListItem,
+    Left,
+    Body,
+    Right,
+    Thumbnail,
+    Text
 } from "native-base";
 
 export default class mainMsgScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Header
-          //   leftComponent={{ icon: "menu", color: "#fff" }}
-          leftComponent={{ text: "D-CHAT", style: { color: "#fff" } }}
-          rightComponent={{ icon: "home", color: "#fff" }}
-        />
-        <Container>
-          <Content>
-            <List>
-              <ListItem avatar>
-                <Left>
-                  <Thumbnail source={{ uri: "Image URL" }} />
-                </Left>
-                <Body>
-                  <Text>John Doe</Text>
-                  <Text note>Lorem ipsum dolar sit amet...</Text>
-                </Body>
-                <Right>
-                  <Text note>3:43 pm</Text>
-                </Right>
-              </ListItem>
-            </List>
-          </Content>
-        </Container>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <Container>
+                    <Content>
+                        <List>
+                            <ListItem avatar>
+                                <Left>
+                                    <Thumbnail source={{ uri: "Image URL" }} />
+                                </Left>
+                                <Body>
+                                    <Text>John Doe</Text>
+                                    <Text note>
+                                        Lorem ipsum dolar sit amet...
+                                    </Text>
+                                </Body>
+                                <Right>
+                                    <Text note>3:43 pm</Text>
+                                </Right>
+                            </ListItem>
+                        </List>
+                    </Content>
+                </Container>
+            </View>
+        );
+    }
 }
